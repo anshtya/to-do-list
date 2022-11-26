d8ad6183ebf493e664e9f71fec495d670d2a056d
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-11-26 19:23:21 +0530

    conflict strategy

--- app/src/main/java/com/example/todolist/database/TodoDao.kt
+++ app/src/main/java/com/example/todolist/database/TodoDao.kt
@@ -3,12 +3,12 @@ package com.example.todolist.database
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
-import androidx.room.OnConflictStrategy.IGNORE
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 
 @Dao
 interface TodoDao {
-    @Insert(onConflict = IGNORE)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(todo: Todo)
     @Delete
     fun delete(todo: Todo)
