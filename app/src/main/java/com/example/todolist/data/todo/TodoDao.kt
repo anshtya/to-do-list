9eeb8fbd19b78b50a96133bcc3abe78ff5cb63c7
Ansh Tyagi <anshtyagi2503@gmail.com>
2022-11-27 21:20:02 +0530

    moved to data

---
+++ app/src/main/java/com/example/todolist/data/todo/TodoDao.kt
@@ -0,0 +1,24 @@
+package com.example.todolist.data.todo
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface TodoDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertTodo(todo: Todo)
+
+    @Delete
+    suspend fun deleteTodo(todo: Todo)
+
+    @Query("SELECT * FROM todo WHERE id = :id")
+    fun getTodo(id: Int): Todo?
+
+    @Query("SELECT * FROM todo ORDER BY todo_name ASC")
+    fun getAll(): Flow<List<Todo>>
+}
